b56e085c5a9ec36450c14d1ca58425d39b950b7c
StraetenMax <maxstraeten@yahoo.com>
2017-05-11 14:14:35 +0200

    création classe Specialite propriétés et constructeurs

---
+++ src/ma/exelib/ecole/Specialite.java
@@ -0,0 +1,22 @@
+package ma.exelib.ecole;
+
+public class Specialite {
+	//propriétés
+	private int id;
+	private String code, libelle;
+	
+	//constructeurs
+	public Specialite(){
+		id = 0;
+		code = "";
+		libelle = "";
+	}
+	public Specialite(int cId, String cCode, String cLibelle){
+		id = cId;
+		code = cCode;
+		libelle = cLibelle;
+	}
+	
+	// accesseurs mutateurs
+	
+}
