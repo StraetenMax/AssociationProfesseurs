25467f2c58c0e1137bae743c8edce6f22afc5c64
StraetenMax <maxstraeten@yahoo.com>
2017-05-11 13:49:00 +0200

    classe Professeur propriétés constructeur

--- src/ma/exelib/ecole/Professeur.java
+++ src/ma/exelib/ecole/Professeur.java
@@ -1,5 +1,27 @@
 package ma.exelib.ecole;
 
 public class Professeur {
-
+    //propriétés
+	private int id;
+	private String nom, prenom, telephone, email;
+	
+	//constructeurs
+	public Professeur(){
+		id = 0;
+		nom = "";
+		prenom = "";
+		telephone = "";
+		email = "";
+	}
+	public Professeur(int cId, String cNom, String cPrenom, String cTelephone, String cEmail){
+		id = cId;
+		nom = cNom;	
+		prenom = cPrenom;
+		telephone = cTelephone;
+		email = cEmail;
+	}
+	
+	//accesseurs mutateurs
+	
+	
 }
